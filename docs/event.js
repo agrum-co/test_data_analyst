a61b3dd67f2261005b4671c6aee35aad5f723018
jp_jurado <jjurado@agrum.co>
2018-08-18 14:36:59 -0500

    Add event description

---
+++ docs/event.js
@@ -0,0 +1,29 @@
+{
+     "work": "5b156b7508fee02c0381073f", //id_trabajo
+     "workgroup": "5aa93b261ad8dc14001530ef", //id_workgroup
+     "temperature_device": 41.75,  //temp_hardware
+     "fix_mode": 1,  //irrelevant
+     "used_sat": 11,  //sat_gps
+     "lat": 3.816952,  //latitude
+     "lng": -76.384913,  //longitude
+     "is_engine_on": false,  //engine of machine 
+     "speed": 0.02,  //speed of machine
+     "time_start": "2018-08-14T11:18:29.000Z",  //timestamp of event
+     "fix": 1,  //gps fix
+     "id_device": "PS-C03",  //id of device
+     "state": "apagado",  //state of machine
+     "type": "machine",  //type of event
+     "hdop": 0.9,  //hdop GPS
+     "timestamp": "2018-08-14T11:18:29.000Z",  //timestamp of event
+     "createdAt_device": "2018-08-14T11:18:30.270Z",  //timestamp of creation on device
+     "finished": false,  //irrelevant
+     "visible_sat": 13,  //gps sat irrelevant
+     "is_ignition_on": false,  //irrelevant
+     "altitude": 955.2,  //gps altitude
+     "createdAt": "2018-08-14T11:18:49.221Z",  //timestamp creation on servers
+     "updatedAt": "2018-08-14T11:18:49.235Z",  //timesamp of update in server
+     "id": "5b72ba86b5c27f3d03d61eb5",  //id of event in server
+     "time_end": "2018-08-14T11:23:29.000Z",  //timestamp of when even ends
+     "duration": 300,  //duration of event
+     "distance": 0  //distance traveled of event
+}
